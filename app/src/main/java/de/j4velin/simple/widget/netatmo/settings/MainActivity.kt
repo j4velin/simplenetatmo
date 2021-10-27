3c7285e80e4138a7f37062c9cf2a9846a50a657d
Thomas Hoffmann <git@j4velin.de>
2021-10-27 12:18:39 +0200

    check for valid stations in MainActivity

--- app/src/main/java/de/j4velin/simple/widget/netatmo/settings/MainActivity.kt
+++ app/src/main/java/de/j4velin/simple/widget/netatmo/settings/MainActivity.kt
@@ -14,9 +14,11 @@ import android.widget.ArrayAdapter
 import de.j4velin.simple.widget.netatmo.GraphWidget
 import de.j4velin.simple.widget.netatmo.R
 import de.j4velin.simple.widget.netatmo.Widget
-import de.j4velin.simple.widget.netatmo.api.getAuthState
+import de.j4velin.simple.widget.netatmo.api.NetatmoWeatherApi
 import de.j4velin.simple.widget.netatmo.api.performAuthentication
 import kotlinx.android.synthetic.main.main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import net.openid.appauth.AuthorizationService
 
 internal const val NOTIFICATION_CHANNEL_ERRORS = "errors"
@@ -48,33 +50,53 @@ class MainActivity : Activity() {
 
     override fun onResume() {
         super.onResume()
-        if (getAuthState(this).isAuthorized) {
-            text.text = getString(R.string.setup_done)
-            authbutton.visibility = View.GONE
-            val widgetPrefs = getSharedPreferences("widgets", Context.MODE_PRIVATE)
-            val graphPrefs = getSharedPreferences("graphwidgets", Context.MODE_PRIVATE)
-            val awm = AppWidgetManager.getInstance(this)
-            widgetIds = awm.getAppWidgetIds(ComponentName(this, Widget::class.java))
-            graphWidgetIds = awm.getAppWidgetIds(ComponentName(this, GraphWidget::class.java))
-            val widgetsNames =
-                widgetIds.map { "#$it - ${widgetPrefs.getString(it.toString() + "_name", "")}" }
-            val graphWidgetsNames = graphWidgetIds.map {
-                "#$it - ${graphPrefs.getString(it.toString() + "_name", "")}"
+        val isAuthorized =
+            NetatmoWeatherApi.getApi(this, { showError(getString(R.string.auth_error, it)) })
+            {
+                GlobalScope.launch {
+                    val devices = it.getStations().body?.devices
+                    runOnUiThread {
+                        if (devices == null || devices.isEmpty()) {
+                            showError(getString(R.string.no_stations))
+                        } else {
+                            setupComplete(devices)
+                        }
+                    }
+                }
             }
-            val adapter =
-                ArrayAdapter(
-                    this, android.R.layout.simple_spinner_dropdown_item,
-                    widgetsNames + graphWidgetsNames
-                )
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            editwidgets.adapter = adapter
-            editbutton.isEnabled = !widgetIds.isEmpty()
-
-        } else {
-            text.text = getString(R.string.setup_need_auth)
-            authbutton.visibility = View.VISIBLE
-            editlayout.visibility = View.GONE
+        if (!isAuthorized) {
+            showError(getString(R.string.setup_need_auth))
         }
+    }
+
+    private fun setupComplete(stations: List<NetatmoWeatherApi.Station>) {
+        text.text = getString(R.string.setup_done)
+        text.append("\nStations: ${stations.map { it.home_name }}")
+        authbutton.visibility = View.GONE
+        val widgetPrefs = getSharedPreferences("widgets", Context.MODE_PRIVATE)
+        val graphPrefs = getSharedPreferences("graphwidgets", Context.MODE_PRIVATE)
+        val awm = AppWidgetManager.getInstance(this)
+        widgetIds = awm.getAppWidgetIds(ComponentName(this, Widget::class.java))
+        graphWidgetIds = awm.getAppWidgetIds(ComponentName(this, GraphWidget::class.java))
+        val widgetsNames =
+            widgetIds.map { "#$it - ${widgetPrefs.getString(it.toString() + "_name", "")}" }
+        val graphWidgetsNames = graphWidgetIds.map {
+            "#$it - ${graphPrefs.getString(it.toString() + "_name", "")}"
+        }
+        val adapter =
+            ArrayAdapter(
+                this, android.R.layout.simple_spinner_dropdown_item,
+                widgetsNames + graphWidgetsNames
+            )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        editwidgets.adapter = adapter
+        editbutton.isEnabled = widgetIds.isNotEmpty()
+    }
+
+    private fun showError(error: String) {
+        text.text = error
+        authbutton.visibility = View.VISIBLE
+        editlayout.visibility = View.GONE
     }
 
     override fun onDestroy() {
